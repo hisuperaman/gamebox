5709f4b0fba5bb3a2d7ea5013a0d602f89762588
Aman <theaman1805@gmail.com>
2024-12-09 21:36:08 +0530

    fixed syntax error

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import git
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('./gamebox')
-            origin = repo.remotes.origin
-            repo.create_head('main', 
-        origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-            origin.pull()
-            return '', 200
-        else:
-            return '', 400
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./gamebox')
+        origin = repo.remotes.origin
+        repo.create_head('main', 
+    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+        origin.pull()
+        return '', 200
+    else:
+        return '', 400
 
 @app.route('/')
 def index():
