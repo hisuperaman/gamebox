ef13c64d502da989673e73a801ce28aeec32d56e
Aman <theaman1805@gmail.com>
2024-12-09 21:16:44 +0530

    added webhook

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, redirect, request, url_for
+import git
 
 app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('./gamebox')
+            origin = repo.remotes.origin
+            repo.create_head('main', 
+        origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+            origin.pull()
+            return '', 200
+        else:
+            return '', 400
 
 @app.route('/')
 def index():
